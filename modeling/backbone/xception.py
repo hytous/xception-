77589297b8ef2b03e37305c168c6474a6a8bd837
hytous <2429598229@qq.com>
2023-04-08 17:03:09 +0800

    美观了一点

--- modeling/backbone/xception.py
+++ modeling/backbone/xception.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import torch.utils.model_zoo as model_zoo
 from modeling.sync_batchnorm.batchnorm import SynchronizedBatchNorm2d
 
+
 def fixed_padding(inputs, kernel_size, dilation):
     kernel_size_effective = kernel_size + (kernel_size - 1) * (dilation - 1)
     pad_total = kernel_size_effective - 1
@@ -95,6 +96,7 @@ class AlignedXception(nn.Module):
     """
     Modified Alighed Xception
     """
+
     def __init__(self, output_stride, BatchNorm,
                  pretrained=True):
         super(AlignedXception, self).__init__()
@@ -109,7 +111,6 @@ class AlignedXception(nn.Module):
             exit_block_dilations = (2, 4)
         else:
             raise NotImplementedError
-
 
         # Entry flow
         self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1, bias=False)
@@ -126,18 +127,18 @@ class AlignedXception(nn.Module):
                             start_with_relu=True, grow_first=True, is_last=True)
 
         # Middle flow
-        self.block4  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
-                             BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
-        self.block5  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
-                             BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
-        self.block6  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
-                             BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
-        self.block7  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
-                             BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
-        self.block8  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
-                             BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
-        self.block9  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
-                             BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block4 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block5 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block6 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block7 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block8 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block9 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
         self.block10 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
                              BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
         self.block11 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
@@ -243,7 +244,6 @@ class AlignedXception(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-
     def _load_pretrained_model(self):
         pretrain_dict = model_zoo.load_url('http://data.lip6.fr/cadene/pretrainedmodels/xception-b5690688.pth')
         model_dict = {}
@@ -278,9 +278,9 @@ class AlignedXception(nn.Module):
         self.load_state_dict(state_dict)
 
 
-
 if __name__ == "__main__":
     import torch
+
     model = AlignedXception(BatchNorm=nn.BatchNorm2d, pretrained=True, output_stride=16)
     input = torch.rand(1, 3, 512, 512)
     output, low_level_feat = model(input)
