91a8cc0a448f46a2bb0c326d86e3bc5bf7e747dc
hytous <2429598229@qq.com>
2023-04-12 15:04:00 +0800

    加了交叉熵数据类型的注释

--- utils/loss.py
+++ utils/loss.py
@@ -20,6 +20,19 @@ class SegmentationLosses(object):
             raise NotImplementedError
 
     def CrossEntropyLoss(self, logit, target):
+        """
+        CrossEntropyLoss交叉熵用的是，图片属于某类的概率
+        也就是说在已知的数据上，某个图片属于它自身的类的概率为1，属于其他类的概率为0
+        预测时某数据属于各个类的概率不同，取概率最大的作为预测值
+
+        在多分类里，如果有0，1，2，3总共4个类，且batch为1
+        某数据为2类，那么它的target的格式为[0, 0, 1, 0]
+        然后预测这个数据的output的格式可能是[0.1, 0.3, 0.6, 0.0]
+        当batch大于1时，target和output就成二维的了
+        比如batch=2，其中第一个数据为0类，第二个数据为3类。那target就是
+        [1, 0, 0, 0]
+        [0, 0, 0, 1]
+        """
         n, c, h, w = logit.size()
         criterion = nn.CrossEntropyLoss(weight=self.weight, ignore_index=self.ignore_index,
                                         size_average=self.size_average)
@@ -27,7 +40,7 @@ class SegmentationLosses(object):
             criterion = criterion.cuda()
 
         # 图片没有为图片分割做好标注，所以先随便填充一下，之后换算法或者给图片弄上标注
-        # changed_target = target.
+        changed_target = target
         changed_target = np.zeros((5, 434, 636))
         for i in range(5):
             changed_target[i][0][0] = target[i]
