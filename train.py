5441a4d9306c2b8e913abef418ed651b42a974d4
hytous <2429598229@qq.com>
2023-04-15 21:31:19 +0800

    改了下存储的地方

--- train.py
+++ train.py
@@ -196,7 +196,8 @@ class Trainer(object):
             self.best_pred = new_pred
             self.saver.save_checkpoint({
                 'epoch': epoch + 1,
-                'state_dict': self.model.module.state_dict(),
+                # 'state_dict': self.model.module.state_dict(),  # 多gpu
+                'state_dict': self.model.state_dict(),
                 'optimizer': self.optimizer.state_dict(),
                 'best_pred': self.best_pred,
             }, is_best)
