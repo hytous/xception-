eef1e077ea1601bd6be168e486228c9e43c4ba34
hytous <2429598229@qq.com>
2023-04-18 12:45:23 +0800

    存储，不能有中文路径

--- utils/saver.py
+++ utils/saver.py
@@ -19,6 +19,10 @@ class Saver(object):
     def save_checkpoint(self, state, is_best, filename='checkpoint.pth.tar'):
         """Saves checkpoint to disk"""
         filename = os.path.join(self.experiment_dir, filename)
+        # filename = os.path.join('D:\S\study\大四\毕设\mol\sav', filename)
+        # filename = os.path.join(r'D:\S\study\four\graduateDesign\mode\checkpoint', filename)
+        # filename = self.experiment_dir
+        print("模型的存储路径是 :", filename)
         torch.save(state, filename)
         if is_best:
             best_pred = state['best_pred']
