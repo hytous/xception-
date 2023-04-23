2a24bf4c9300c7ba1410c18c7bce3a82ab3278e7
hytous <2429598229@qq.com>
2023-04-23 18:38:37 +0800

    改了验证集和训练集的选取

--- dataloaders/datasets/fattyliver.py
+++ dataloaders/datasets/fattyliver.py
@@ -9,14 +9,18 @@ import scipy.io as scio
 img_chang = 434
 img_kuan = 636
 
+
 def get_data():  # 获取数据
+    val_index = [34, 42, 23, 2, 12]  # 验证集的下表
     train_data = []
     train_label = []
     val_data = []
     val_label = []
     # 可以用matlab查看数据形式
+    # allmatdatas = scio.loadmat(
+    #     r'../dataset_liver_bmodes_steatosis_assessment_IJCARS.mat')  # 在本文件内运行时的相对路径
     allmatdatas = scio.loadmat(
-        r'./dataloaders/dataset_liver_bmodes_steatosis_assessment_IJCARS.mat')
+        r'./dataloaders/dataset_liver_bmodes_steatosis_assessment_IJCARS.mat')  # 在train文件内运行时的相对路径
     matdatas = allmatdatas['data']
     matdatas = matdatas[0]
     for i, matdata in enumerate(matdatas):  # 总共55组图片  enumerate可以在for里多加一个计数器i
@@ -25,17 +29,19 @@ def get_data():  # 获取数据
         # 先做四分类
         if fat < 5:
             fatclass = 0  # 无脂肪肝
-        elif 5 <= fat <= 24:
+        elif 5 <= fat <= 27:
             fatclass = 1  # 轻度脂肪肝
-        elif 25 <= fat <= 44:
+        elif 28 <= fat <= 55:
             fatclass = 2  # 中度脂肪肝
-        elif 45 <= fat:
+        elif 56 <= fat:
             fatclass = 3  # 重度脂肪肝
 
         img_batch = matdata['images']  # 每组图片为10张434*636的灰度图
+        # print('编号为%d的病人fat程度%d  fat种类%d' % (i, fat, fatclass))
         try:
-            if i % 11 == 0:  # 隔11个数据抽一组作为验证集，验证机中总共5组
+            if i in val_index:  # 隔11个数据抽一组作为验证集，验证机中总共5组
                 # val_data.append([img_batch.astype(np.float32), fatclass])  # 原始图片数据是int类型的，之后的处理需要float类型
+                # print('验证集fat程度%d  fat种类%d' % (fat, fatclass))
                 for img in img_batch:
                     img = np.expand_dims(img, axis=0)  # 增维
                     val_data.append(img.astype(np.float32))  # 原始图片数据是int类型的，之后的处理需要float类型
@@ -53,7 +59,6 @@ def get_data():  # 获取数据
 
 # 定义FattyLiver类，继承Dataset方法，并重写__getitem__()和__len__()方法
 class FattyLiver(Dataset):
-
     # 总共四个类
     NUM_CLASSES = 4
 
@@ -100,10 +105,30 @@ if __name__ == "__main__":
     print("val_data的shape", val_data.shape)
     print("val_label的shape", val_label.shape)
     # 通过FattyLiver将数据进行加载，返回Dataset对象，包含data和labels
-    torch_data = FattyLiver('train')
+    # torch_data = FattyLiver('train')
 
     # 通过上述的程序，我们构造了一个数据加载器torch_data，但是还是不能直接
     # 传入网络中。接下来需要构造数据装载器，产生可迭代的数据，再传入网络中。DataLoader类完成这个工作。
     # num_workers : 表示加载的时候子进程数
-    dataloader = DataLoader(torch_data, batch_size=5, shuffle=True, drop_last=False, num_workers=0)
-
+    # dataloader = DataLoader(torch_data, batch_size=5, shuffle=True, drop_last=False, num_workers=0)
+"""
+fat程度为0的共2人
+fat程度为1的共3人
+fat程度为2的共8人
+fat程度为3的共3人
+fat程度为4的共1人
+fat程度为5的共1人
+fat程度为7的共1人
+fat程度为10的共4人
+fat程度为15的共3人
+fat程度为20的共7人
+fat程度为25的共3人
+fat程度为30的共1人
+fat程度为40的共3人
+fat程度为50的共3人
+fat程度为55的共2人
+fat程度为70的共5人
+fat程度为75的共2人
+fat程度为80的共2人
+fat程度为85的共1人
+"""
