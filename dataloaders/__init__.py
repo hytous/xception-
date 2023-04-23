19d870b336b70b3d94e84716b27df2fb507c271e
hytous <2429598229@qq.com>
2023-04-23 18:36:40 +0800

    加载数据集时多返回一个各类名称

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -6,13 +6,14 @@ from torch.utils.data import DataLoader
 def make_data_loader(args, **kwargs):
 
     if args.dataset == 'fattyliver':
+        class_names = ['health', 'mild', 'moderate', 'severe']  # 健康 轻度 中度 重度
         train_set = fattyliver.FattyLiver(args, split='train')
         val_set = fattyliver.FattyLiver(args, split='val')
         num_class = train_set.NUM_CLASSES
         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
         test_loader = None
-        return train_loader, val_loader, test_loader, num_class
+        return train_loader, val_loader, test_loader, num_class, class_names
 
     else:
         raise NotImplementedError
