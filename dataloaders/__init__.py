d1e2b0e6f65d7f934fb00b027ea5daf8a6cbb626
hytous <2429598229@qq.com>
2023-04-24 01:35:15 +0800

    没进行shuffle

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -9,11 +9,13 @@ def make_data_loader(args, **kwargs):
         class_names = ['health', 'mild', 'moderate', 'severe']  # 健康 轻度 中度 重度
         train_set = fattyliver.FattyLiver(args, split='train')
         val_set = fattyliver.FattyLiver(args, split='val')
+        all_set = fattyliver.FattyLiver(args, split='all')
         num_class = train_set.NUM_CLASSES
-        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+        # train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
-        test_loader = None
-        return train_loader, val_loader, test_loader, num_class, class_names
+        all_loader = DataLoader(all_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+        return train_loader, val_loader, all_loader, num_class, class_names
 
     else:
         raise NotImplementedError
