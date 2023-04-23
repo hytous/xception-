c65fafeb6acce4f8ec5bfbd18ac56dbf8b53070a
hytous <2429598229@qq.com>
2023-04-23 18:37:12 +0800

    改了缩进

--- modeling/backbone/__init__.py
+++ modeling/backbone/__init__.py
@@ -1,13 +1,14 @@
 from modeling.backbone import resnet, xception, drn, mobilenet
 
-def build_backbone(backbone, output_stride, BatchNorm):
+
+def build_backbone(backbone, output_stride, BatchNorm, pretrained=True):
     if backbone == 'resnet':
-        return resnet.ResNet101(output_stride, BatchNorm)
+        return resnet.ResNet101(output_stride, BatchNorm, pretrained)
     elif backbone == 'xception':
-        return xception.AlignedXception(output_stride, BatchNorm)
+        return xception.AlignedXception(output_stride, BatchNorm, pretrained)
     elif backbone == 'drn':
         return drn.drn_d_54(BatchNorm)
     elif backbone == 'mobilenet':
-        return mobilenet.MobileNetV2(output_stride, BatchNorm)
+        return mobilenet.MobileNetV2(output_stride, BatchNorm, pretrained=pretrained)
     else:
         raise NotImplementedError
