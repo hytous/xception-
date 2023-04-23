1dccc1444929df49db29cab147da3ee3773faccc
hytous <2429598229@qq.com>
2023-04-24 01:35:52 +0800

    课返回全部数据集

--- dataloaders/datasets/fattyliver.py
+++ dataloaders/datasets/fattyliver.py
@@ -31,23 +31,22 @@ def get_data():  # 获取数据
             fatclass = 0  # 无脂肪肝
         elif 5 <= fat <= 27:
             fatclass = 1  # 轻度脂肪肝
-        elif 28 <= fat <= 55:
+        elif 28 <= fat <= 45:  # 55
             fatclass = 2  # 中度脂肪肝
-        elif 56 <= fat:
+        elif 46 <= fat:  # 56
             fatclass = 3  # 重度脂肪肝
 
         img_batch = matdata['images']  # 每组图片为10张434*636的灰度图
         # print('编号为%d的病人fat程度%d  fat种类%d' % (i, fat, fatclass))
         try:
-            if i in val_index:  # 隔11个数据抽一组作为验证集，验证机中总共5组
-                # val_data.append([img_batch.astype(np.float32), fatclass])  # 原始图片数据是int类型的，之后的处理需要float类型
+            # if i in val_index:  # 隔11个数据抽一组作为验证集，验证机中总共5组
+            if i % 11 == 0:
                 # print('验证集fat程度%d  fat种类%d' % (fat, fatclass))
                 for img in img_batch:
                     img = np.expand_dims(img, axis=0)  # 增维
                     val_data.append(img.astype(np.float32))  # 原始图片数据是int类型的，之后的处理需要float类型
                     val_label.append(fatclass)
             else:
-                # train_data.append([img_batch.astype(np.float32), fatclass])  # 原始图片数据是int类型的，之后的处理需要float类型
                 for img in img_batch:
                     img = np.expand_dims(img, axis=0)  # 增维
                     train_data.append(img.astype(np.float32))  # 原始图片数据是int类型的，之后的处理需要float类型
@@ -57,6 +56,40 @@ def get_data():  # 获取数据
     return np.array(train_data), np.array(train_label), np.array(val_data), np.array(val_label)
 
 
+def get_all_data():  # 获取数据
+    all_data = []
+    all_label = []
+    # allmatdatas = scio.loadmat(
+    #     r'../dataset_liver_bmodes_steatosis_assessment_IJCARS.mat')  # 在本文件内运行时的相对路径
+    allmatdatas = scio.loadmat(
+        r'./dataloaders/dataset_liver_bmodes_steatosis_assessment_IJCARS.mat')  # 在train文件内运行时的相对路径
+    matdatas = allmatdatas['data']
+    matdatas = matdatas[0]
+    for i, matdata in enumerate(matdatas):  # 总共55组图片  enumerate可以在for里多加一个计数器i
+        # fatclass = matdata['class'][0, 0]  # 是否是脂肪肝，用0或1表示 [0,0]是因为数据格式是：[[0]]这样的，可能是tensor吧
+        fat = matdata['fat'][0, 0]
+        # 先做四分类
+        if fat < 5:
+            fatclass = 0  # 无脂肪肝
+        elif 5 <= fat <= 27:
+            fatclass = 1  # 轻度脂肪肝
+        elif 28 <= fat <= 45:  # 55
+            fatclass = 2  # 中度脂肪肝
+        elif 46 <= fat:  # 56
+            fatclass = 3  # 重度脂肪肝
+
+        img_batch = matdata['images']  # 每组图片为10张434*636的灰度图
+        # print('编号为%d的病人fat程度%d  fat种类%d' % (i, fat, fatclass))
+        try:
+            for img in img_batch:
+                img = np.expand_dims(img, axis=0)  # 增维
+                all_data.append(img.astype(np.float32))  # 原始图片数据是int类型的，之后的处理需要float类型
+                all_label.append(fatclass)
+        except Exception as e:
+            print(e)
+    return np.array(all_data), np.array(all_label)
+
+
 # 定义FattyLiver类，继承Dataset方法，并重写__getitem__()和__len__()方法
 class FattyLiver(Dataset):
     # 总共四个类
@@ -64,17 +97,24 @@ class FattyLiver(Dataset):
 
     def __init__(self, args, split='train'):
         super().__init__()
-        train_data, train_label, val_data, val_label = get_data()
 
         # 初始化函数，得到数据
         if split == 'train':
+            train_data, train_label, val_data, val_label = get_data()
             img_batch = train_data
             img_batch_label = train_label
             self.data = img_batch
             self.label = img_batch_label
         elif split == 'val':
+            train_data, train_label, val_data, val_label = get_data()
             img_batch = val_data
             img_batch_label = val_label
+            self.data = img_batch
+            self.label = img_batch_label
+        elif split == 'all':
+            all_data, all_label = get_all_data()
+            img_batch = all_data
+            img_batch_label = all_label
             self.data = img_batch
             self.label = img_batch_label
         self.split = split
