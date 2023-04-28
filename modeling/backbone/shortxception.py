825f383225dcbe25449bb984a6c7e1f51949e351
hytous <2429598229@qq.com>
2023-04-28 21:03:03 +0800

    缩小xception网络

---
+++ modeling/backbone/shortxception.py
@@ -0,0 +1,348 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+from modeling.sync_batchnorm.batchnorm import SynchronizedBatchNorm2d
+
+
+def fixed_padding(inputs, kernel_size, dilation):
+    kernel_size_effective = kernel_size + (kernel_size - 1) * (dilation - 1)
+    pad_total = kernel_size_effective - 1
+    pad_beg = pad_total // 2
+    pad_end = pad_total - pad_beg
+    padded_inputs = F.pad(inputs, (pad_beg, pad_end, pad_beg, pad_end))
+    return padded_inputs
+
+
+class SeparableConv2d(nn.Module):
+    def __init__(self, inplanes, planes, kernel_size=3, stride=1, dilation=1, bias=False, BatchNorm=None):
+        super(SeparableConv2d, self).__init__()
+
+        self.conv1 = nn.Conv2d(inplanes, inplanes, kernel_size, stride, 0, dilation,
+                               groups=inplanes, bias=bias)
+        self.bn = BatchNorm(inplanes)
+        self.pointwise = nn.Conv2d(inplanes, planes, 1, 1, 0, 1, 1, bias=bias)
+
+    def forward(self, x):
+        x = fixed_padding(x, self.conv1.kernel_size[0], dilation=self.conv1.dilation[0])
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = self.pointwise(x)
+        return x
+
+
+class Block(nn.Module):
+    # reps表示此block里有几个相同的relu+SeparableConv2d，然后用for进行堆叠避免代码冗余
+    # 这里的stride只有在最后的最大池化层用到，所以这个stride是表示池化的stride
+    # grow表示是先改变通道数还是最后改变通道数
+    def __init__(self, inplanes, planes, reps, stride=1, dilation=1, BatchNorm=None,
+                 start_with_relu=True, grow_first=True, is_last=False):
+        super(Block, self).__init__()
+
+        # 残差（RES）  如果此block对的训练起不到增益就跳过此block，但是如果图片输入输出通道数或图片大小不一致的话要进行放缩操作,防止输入下一block时不满足输入要求
+        # 加入残差可以使网络收敛更快，绘图时表现出的曲线也更平滑
+        if planes != inplanes or stride != 1:  # 输入通道数不等于输出通道数，或者卷积步长不为1，说明要改变通道数或者改变图片大小
+            # 因为只有最大池化层会按照输入的stride改一次图像大小，所以RES只需要按照输入的stride改一次图像大小
+            # block的通道数只会改变一次所以res也只需改一次
+            self.skip = nn.Conv2d(inplanes, planes, 1, stride=stride, bias=False)
+            self.skipbn = BatchNorm(planes)
+        else:
+            self.skip = None
+
+        self.relu = nn.ReLU(inplace=True)  # 设置激活函数，增加非线性
+        rep = []
+
+        filters = inplanes
+        # 如果先改变通道数
+        if grow_first:
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(inplanes, planes, 3, 1, dilation, BatchNorm=BatchNorm))
+            rep.append(BatchNorm(planes))
+            filters = planes
+
+        for i in range(reps - 1):
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(filters, filters, 3, 1, dilation, BatchNorm=BatchNorm))
+            rep.append(BatchNorm(filters))
+
+        # Entry flow的第一个block没有先relu，所以去掉第一个relu层
+        if not grow_first:
+            # 如果后改变通道数
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(inplanes, planes, 3, 1, dilation, BatchNorm=BatchNorm))
+            rep.append(BatchNorm(planes))
+
+        # 最大池化层MaxPooling
+        if stride != 1:
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(planes, planes, 3, 2, BatchNorm=BatchNorm))
+            rep.append(BatchNorm(planes))
+
+        if stride == 1 and is_last:
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(planes, planes, 3, 1, BatchNorm=BatchNorm))
+            rep.append(BatchNorm(planes))
+
+        if not start_with_relu:
+            rep = rep[1:]
+
+        #  torch.nn.Sequential是一个Sequential容器,模块将按照构造函数中传递的顺序添加到模块中。另外，也可以传入一个有序模块。
+        self.rep = nn.Sequential(*rep)
+
+    def forward(self, inp):
+        x = self.rep(inp)
+
+        if self.skip is not None:
+            skip = self.skip(inp)
+            skip = self.skipbn(skip)
+        else:
+            skip = inp
+
+        # 残差分支和主干分支相加
+        x = x + skip
+
+        return x
+
+
+class ShortXception(nn.Module):
+    """
+    Modified Alighed Xception
+    """
+    # 因为多显卡和但显卡训练的BatchNorm不同，所以要从外部决定用哪种，然后输入进来用。避免在这个代码里大量的修改。
+    def __init__(self, output_stride, BatchNorm, pretrained=True):
+        super(ShortXception, self).__init__()
+
+        if output_stride == 16:
+            entry_block3_stride = 2
+            middle_block_dilation = 1
+            exit_block_dilations = (1, 2)
+        elif output_stride == 8:
+            entry_block3_stride = 1
+            middle_block_dilation = 2
+            exit_block_dilations = (2, 4)
+        else:
+            raise NotImplementedError
+
+        # Entry flow
+        # self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=2, padding=1, bias=False)  # 输入通道数改成1
+        self.bn1 = BatchNorm(32)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=1, bias=False)
+        self.bn2 = BatchNorm(64)
+
+        self.block1 = Block(64, 128, reps=2, stride=2, BatchNorm=BatchNorm, start_with_relu=False)
+        self.block2 = Block(128, 256, reps=2, stride=2, BatchNorm=BatchNorm, start_with_relu=False,
+                            grow_first=True)
+        self.block3 = Block(256, 728, reps=2, stride=entry_block3_stride, BatchNorm=BatchNorm,
+                            start_with_relu=True, grow_first=True, is_last=True)
+
+        # Middle flow
+        self.block4 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block5 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block6 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block7 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+                            BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        # self.block8 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+        #                     BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        # self.block9 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+        #                     BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        # self.block10 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+        #                      BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        # self.block11 = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
+        #                      BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
+        self.block20 = Block(728, 1024, reps=2, stride=2, dilation=exit_block_dilations[0],
+                             BatchNorm=BatchNorm, start_with_relu=True, grow_first=False, is_last=True)
+
+        self.conv3 = SeparableConv2d(1024, 1536, 3, stride=1, dilation=exit_block_dilations[1], BatchNorm=BatchNorm)
+        self.bn3 = BatchNorm(1536)
+
+        # self.conv4 = SeparableConv2d(1536, 1536, 3, stride=1, dilation=exit_block_dilations[1], BatchNorm=BatchNorm)
+        # self.bn4 = BatchNorm(1536)
+
+        self.conv5 = SeparableConv2d(1536, 2048, 3, stride=1, dilation=exit_block_dilations[1], BatchNorm=BatchNorm)
+        self.bn5 = BatchNorm(2048)
+
+        # 缺一个全局平均池化
+        # 全连接层
+        self.fc = nn.Linear(2048, 4, bias=False)  # 2048通道对应到4类
+
+        # Init weights
+        self._init_weight()
+
+        # Load pretrained model
+        if pretrained:
+            self._load_pretrained_model()
+
+    def forward(self, x):
+        # Entry flow
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.block1(x)
+        # add relu here
+        x = self.relu(x)
+        low_level_feat = x
+        x = self.block2(x)
+        x = self.block3(x)
+
+        # Middle flow
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.block7(x)
+        # x = self.block8(x)
+        # x = self.block9(x)
+        # x = self.block10(x)
+        # x = self.block11(x)
+        # x = self.block12(x)
+        # x = self.block13(x)
+        # x = self.block14(x)
+        # x = self.block15(x)
+        # x = self.block16(x)
+        # x = self.block17(x)
+        # x = self.block18(x)
+        # x = self.block19(x)
+        # print("middleflow的输出: ", x.size())  # [batch, 728, 32, 32])
+
+        # Exit flow
+        x = self.block20(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+
+        # x = self.conv4(x)
+        # x = self.bn4(x)
+        # x = self.relu(x)
+
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = self.relu(x)  # [batch_size, 2048, 28, 40]
+        # print("512大小的时候输出的x的size", x.size())  # [batch_size, 2048, 32, 32])
+        # mat1 and mat2 shapes cannot be multiplied (286720x40 and 2048x4)
+
+        # 全局平均池化
+        # 参考了
+        # https://github.com/Cadene/pretrained-models.pytorch/blob/master/pretrainedmodels/models/xception.py
+        # 里的logits函数
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        # print("全局平均池化之后是这样的: ", x.size())  # [batch_size, 2048, 1, 1]
+
+        # x.view()就是对tensor进行reshape。将向量铺平，便于传入全连接层
+        # view里的 -1 表示一个不确定的数，就是你如果不确定你想要reshape成几列，但是你很肯定要reshape成batch_size（即x.size(0)）行，
+        # 那不确定的地方就可以写成 - 1
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)  # 全连接层
+        # print("全连接完是这样的: ", x.size())  # torch.Size([5, 4])
+        return x, low_level_feat
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, SynchronizedBatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _load_pretrained_model(self):
+        #  autodl的与服务器存在/root/.cache/torch/hub/checkpoints/文件夹里
+        #  预训练模型可能会下坏掉，坏掉得手动去删掉重新下
+        pretrain_dict = model_zoo.load_url('http://data.lip6.fr/cadene/pretrainedmodels/xception-b5690688.pth')
+        model_dict = {}
+        state_dict = self.state_dict()
+
+        my_change_flag = 0  # 用来修改第一层的输入通道数
+        for k, v in pretrain_dict.items():
+            if k in state_dict:
+                if 'pointwise' in k:
+                    v = v.unsqueeze(-1).unsqueeze(-1)
+                if my_change_flag == 0:  # 改一下第一层的输入通道数
+                    model_dict[k] = v.resize_([32, 1, 3, 3])
+                    my_change_flag = 1
+                if k.startswith('fc.weight'):  # 全连接层预训练模型是[1000, 2048]，原模型有1000个类
+                    model_dict[k] = v.resize_([4, 2048])
+                # if k.startswith('block11'):
+                #     model_dict[k] = v
+                #     model_dict[k.replace('block11', 'block12')] = v
+                #     model_dict[k.replace('block11', 'block13')] = v
+                #     model_dict[k.replace('block11', 'block14')] = v
+                #     model_dict[k.replace('block11', 'block15')] = v
+                #     model_dict[k.replace('block11', 'block16')] = v
+                #     model_dict[k.replace('block11', 'block17')] = v
+                #     model_dict[k.replace('block11', 'block18')] = v
+                #     model_dict[k.replace('block11', 'block19')] = v
+                # elif k.startswith('block12'):
+                #     model_dict[k.replace('block12', 'block20')] = v
+                # elif k.startswith('bn3'):
+                #     model_dict[k] = v
+                #     model_dict[k.replace('bn3', 'bn4')] = v
+                elif k.startswith('conv4'):
+                    model_dict[k.replace('conv4', 'conv5')] = v
+                elif k.startswith('bn4'):
+                    model_dict[k.replace('bn4', 'bn5')] = v
+                else:
+                    model_dict[k] = v
+        state_dict.update(model_dict)
+        self.load_state_dict(state_dict)
+
+    # 返回每层需要被训练的参数
+    def get_1x_lr_params(self):
+        modules = [self.backbone]
+        for i in range(len(modules)):
+            # modules[0]里存了所有层的名称、参数等信息
+            for m in modules[i].named_modules():  # modules存的是各个层的名称、参数信息、权重等要被训练的东西,modules[0]有所有层的信息
+                if self.freeze_bn:
+                    if isinstance(m[1], nn.Conv2d):
+                        # 输出m[1]是: 层的类别(参数)
+                        # 比如：Conv2d(10, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+                        for p in m[1].parameters():
+                            # 关于.requires_grad， 需要保留该tensor的梯度信息，用于前向传播。保留的就是要被训练的，不保留的没法训练
+                            # https://blog.csdn.net/weixin_44696221/article/details/104269981
+                            if p.requires_grad:  # 要被训练
+                                yield p
+                else:
+                    if isinstance(m[1], nn.Conv2d) or isinstance(m[1], SynchronizedBatchNorm2d) \
+                            or isinstance(m[1], nn.BatchNorm2d):
+                        for p in m[1].parameters():
+                            if p.requires_grad:
+                                yield p
+
+if __name__ == "__main__":
+    import torch
+
+    model = ShortXception(BatchNorm=nn.BatchNorm2d, pretrained=False, output_stride=16)
+    # input = torch.rand(1, 3, 512, 512)
+    # input = torch.rand(5, 10, 512, 512)
+    input = torch.rand(5, 1, 434, 636)
+    output, low_level_feat = model(input)
+    # modules = [model]
+    # # print(len(modules))  # 用来理解get_1x_lr_params在干嘛
+    # for i in range(len(modules)):
+    #     for m in modules[i].named_modules():  # modules存的是各个层的名称、参数信息,modules[0]是所有层的信息
+    #         # print("m是: ", m)
+    #         # m[1]是层的类别(参数)
+    #         # 诸如：Conv2d(10, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False) 这样的
+    #         if isinstance(m[1], nn.Conv2d):
+    #             # for p in m[1].parameters():
+    #             #     print("p是:", p)
+    #             #     if p.requires_grad:
+    #             #         yield p
+
+    # print(output.size())  # [5, 4]
+    # print(low_level_feat.size())  # [5, 128, 128, 128]
+
